8dcb32010124bcbec3a06d92bfb82c18fac115f8
Romain Graux <romain.graux@student.uclouvain.be>
2019-10-10 19:18:21 +0200

    Good discussion IPV6

--- src/receive.c
+++ src/receive.c
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h> // sockaddr_in6
 #include <netdb.h> // addrinfo
+#include <errno.h>
 
 #include <arpa/inet.h>
 
@@ -18,7 +19,7 @@ int IPV6_translater(const char* hostname, struct sockaddr_in6 *ipv6){
     struct addrinfo hints, *ai;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family   = AF_INET6; // IPv6
-    hints.ai_socktype = SOCK_DGRAM; // Datagrams
+    hints.ai_socktype = 0; // Datagrams
     hints.ai_protocol = IPPROTO_UDP; // UDP
     hints.ai_flags    = 0; // Pas de spécificité
     if ((status = getaddrinfo(hostname , NULL, &hints, &ai )) != 0) {
@@ -26,15 +27,15 @@ int IPV6_translater(const char* hostname, struct sockaddr_in6 *ipv6){
         return -1;
     }
     ipv6 = (struct sockaddr_in6 *)ai->ai_addr;
+    char *ipstr = malloc(INET6_ADDRSTRLEN);
 
-    if(print == 0){
     void *addr;
-    char ipstr[INET6_ADDRSTRLEN];
     addr = &(ipv6->sin6_addr);
     char ipver = '6';
     inet_ntop(ai->ai_family, addr, ipstr, sizeof ipstr);
     printf(" IPv%c: %s\n", ipver, ipstr);
-    }
+
+    inet_pton(AF_INET6, ipstr, &(ipv6->sin6_addr));
 
     freeaddrinfo(ai);
     return 0;
@@ -43,14 +44,15 @@ int IPV6_translater(const char* hostname, struct sockaddr_in6 *ipv6){
 int socket_init(struct sockaddr_in6 *src_addr, const int src_port,
                 struct sockaddr_in6 *dest_addr, const int dest_port){
 
-    int sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP); // IPv6, datagrams, UDP
+
+    int sockfd = socket(AF_INET6, SOCK_DGRAM, 0); // IPv6, datagrams, UDP
     if(sockfd == -1){fprintf(stderr, "[socket_init] Impossible de créer un socket.\n"); return -1;}
 
     // Bind l'adresse source avec le socket
-    if(src_addr != NULL && src_port > 0){
-        src_addr->sin6_port = htons(src_port);
-        if(bind(sockfd,(const struct sockaddr *)src_addr, (socklen_t)sizeof(struct sockaddr_in6)) < 0){ fprintf(stderr, "[socket_init] Impossible de bind la source \n"); return -1;}
-    }
+    // if(src_addr != NULL && src_port > 0){
+    //     src_addr->sin6_port = htons(src_port);
+    //     if(bind(sockfd,(const struct sockaddr *)src_addr, sizeof(struct sockaddr_in6)) < 0){ printf("%s\n", strerror(errno));fprintf(stderr, "[socket_init] Impossible de bind la source \n"); return -1;}
+    // }
 
     // Connect l'adresse de destination avec le socket
     if(dest_addr != NULL && dest_port > 0){
