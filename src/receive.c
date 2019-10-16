b5fb103e8ae62a83ea6a06463dc77b306ac9f7ec
Romain Graux <romain.graux@student.uclouvain.be>
2019-10-16 12:46:26 +0200

    Traitement de fichier de sortie avec un tableau contenant les file descriptor aux indices des clients

--- src/receive.c
+++ src/receive.c
@@ -4,15 +4,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> // write, close
+// #include <sys/types.h>
+// #include <sys/stat.h>
+#include <fcntl.h> // open
 #include <string.h> // memset
 #include <sys/socket.h>
 #include <netinet/in.h> // sockaddr_in6
 #include <netdb.h> // addrinfo
 #include <errno.h>
-
 #include <arpa/inet.h>
 
 int print = 0;
+int iterator_file = 0, len_format = 0, max_connections; // max_connections == -1 si pas de limite
+char * format = NULL;
+int * file_descriptors = NULL;
+
+
 
 int IPV6_translater(const char* hostname, struct sockaddr_in6 *ipv6){
     int status;
@@ -63,6 +71,39 @@ int socket_init(struct sockaddr_in6 *src_addr, const int src_port,
     return sockfd;
 }
 
-int receive(char* hostname, int port, char* filename){
-    return EXIT_SUCCESS;
+
+int receive(int connections, char * hostname, int port, char * main_format){
+    max_connections = connections;
+    format = malloc(sizeof(char)*strlen(main_format));
+    strcpy(format, main_format);
+    len_format = strlen(format) + 4;
+    if (max_connections != -1) {
+        file_descriptors = malloc(sizeof(int) * max_connections);
+    }
+    if(file_descriptors!= NULL){free(file_descriptors);}
+    return 0;
 }
+
+int openFile(){
+    char filename[len_format];
+    int err = snprintf(filename, len_format, format, iterator_file);
+    int filefd = open(filename, O_WRONLY|O_CREAT|O_TRUNC);
+    if(filefd == -1){fprintf(stderr, "[openFile] : %s\n", strerror(errno)); return -1;}
+    file_descriptors[iterator_file] = filefd;
+    iterator_file ++;
+    return 0;
+}
+
+int closeFile(const int index){
+    close(file_descriptors[index]);
+    return 0;
+}
+
+int writeFile(const int index, const char * buf, size_t len){
+    return write(file_descriptors[index], buf, len);
+}
+
+// int printfile(char * filename, char * data, int len, int connection){
+//     int fd, err;
+//     fd = open(open("foo.txt", O_RDONLY | O_CREAT);  )
+// }
